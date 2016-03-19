17f69e7a2e77c0f1387f2eabd91922205b4a9037
John Shea <john@johnshea.nyc>
2016-03-19 13:49:12 -0400

    fixing routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -66,6 +66,7 @@ class UsersController < ApplicationController
     end
   end
 
+  # will need to uncomment the middle part of this method before launching in production
   def handle_ip
     # Prevent someone from gaming the site by referring themselves.
     # Presumably, users are doing this from the same device so block
@@ -77,10 +78,10 @@ class UsersController < ApplicationController
     current_ip = IpAddress.find_by_address(address)
     if current_ip.nil?
       current_ip = IpAddress.create(address: address, count: 1)
-    elsif current_ip.count > 2
-      logger.info('IP address has already appeared three times in our records.
-                 Redirecting user back to landing page.')
-      return redirect_to root_path
+    # elsif current_ip.count > 2
+    #   logger.info('IP address has already appeared three times in our records.
+    #              Redirecting user back to landing page.')
+    #   return redirect_to root_path
     else
       current_ip.count += 1
       current_ip.save
