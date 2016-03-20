8f368577e4079684196a06d3282cf9d0e2615029
John Shea <john@johnshea.nyc>
2016-03-20 12:30:06 -0400

    referrer ids not hitting database correctly

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ActionMailer::Base
-  default from: "Harry's <welcome@harrys.com>"
+  default from: "Harry's <ohhello@verilymag.com>"
 
   def signup_email(user)
     @user = user
-    @twitter_message = "#Shaving is evolving. Excited for @harrys to launch."
+    @twitter_message = "Verily Magazine. Less of who you should be, more of who you are."
 
     mail(:to => user.email, :subject => "Thanks for signing up!")
   end
--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,15 @@ Prelaunchr::Application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
 
-  resources :users
-
   root :to => "users#new"
 
   post 'users/create' => 'users#create'
   get 'refer-a-friend' => 'users#refer'
+
   post 'refer-a-friend' => 'users#refer'
   get 'faq' => 'users#faq'
+
+  resources :users, :only => [:show, :update, :edit] 
 
   unless Rails.application.config.consider_all_requests_local
     get '*not_found', to: 'users#redirect', :format => false
